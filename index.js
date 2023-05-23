04c70a5507d973e89fd122cea58ce21f22e10a1f
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2023-05-23 12:03:50 +0530

    adding two file index.js and index.css for button styling and function

---
+++ index.js
@@ -0,0 +1,11 @@
+const button = document.getElementById("start");
+const list = document.getElementById("list");
+list.style.display = "none";
+button.addEventListener("click", (event) => {
+    if (list.style.display == "none") {
+        list.style.display = "block";   
+    }
+    else{
+        list.style.display = "none";
+    }
+})
